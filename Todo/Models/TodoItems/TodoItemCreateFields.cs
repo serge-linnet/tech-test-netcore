b00c0d15389f371fcbe669350d7f16120f66fe03
serge <serge@outlook.com>
2024-06-28 12:12:02 +0100

    #4 Fixed responsible party and done fields display names

--- Todo/Models/TodoItems/TodoItemCreateFields.cs
+++ Todo/Models/TodoItems/TodoItemCreateFields.cs
@@ -1,13 +1,19 @@
-﻿using Todo.Data.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using Todo.Data.Entities;
 
 namespace Todo.Models.TodoItems
 {
     public class TodoItemCreateFields
     {
         public int TodoListId { get; set; }
+        
         public string Title { get; set; }
+        
         public string TodoListTitle { get; set; }
+
+        [Display(Name = "Responsible Party")]
         public string ResponsiblePartyId { get; set; }
+
         public Importance Importance { get; set; } = Importance.Medium;
 
         public TodoItemCreateFields() { }
--- Todo/Models/TodoItems/TodoItemEditFields.cs
+++ Todo/Models/TodoItems/TodoItemEditFields.cs
@@ -1,15 +1,24 @@
-﻿using Todo.Data.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using Todo.Data.Entities;
 
 namespace Todo.Models.TodoItems
 {
     public class TodoItemEditFields
     {
         public int TodoListId { get; set; }
+
         public string Title { get; set; }
+
         public string TodoListTitle { get; set; }
+
         public int TodoItemId { get; set; }
+
+        [Display(Name = "Is Done")]
         public bool IsDone { get; set; }
+
+        [Display(Name = "Responsible Party")]
         public string ResponsiblePartyId { get; set; }
+
         public Importance Importance { get; set; }
 
         public TodoItemEditFields() { }
