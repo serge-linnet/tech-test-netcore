0e2652f21017243a71e9ba96e87ad42580f4746c
serge <serge@outlook.com>
2024-06-28 11:42:04 +0100

    #2 Sorted list items by importance

--- Todo/Services/ApplicationDbContextConvenience.cs
+++ Todo/Services/ApplicationDbContextConvenience.cs
@@ -17,7 +17,8 @@ namespace Todo.Services
         public static TodoList SingleTodoList(this ApplicationDbContext dbContext, int todoListId)
         {
             return dbContext.TodoLists.Include(tl => tl.Owner)
-                .Include(tl => tl.Items)
+                .Include(tl => 
+                    tl.Items.OrderBy(i => i.Importance))
                 .ThenInclude(ti => ti.ResponsibleParty)
                 .Single(tl => tl.TodoListId == todoListId);
         }
